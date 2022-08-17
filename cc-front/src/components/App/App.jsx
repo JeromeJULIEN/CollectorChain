6b36cb690fe6444420bb62be5d9fecf97c6fd159
JeromeJULIEN <102427619+JeromeJULIEN@users.noreply.github.com>
2022-08-17 17:05:09 +0200

    init routes dans App

--- cc-front/src/components/App/App.jsx
+++ cc-front/src/components/App/App.jsx
@@ -1,18 +1,19 @@
 import { useState } from "react";
-import reactLogo from "../../assets/react.svg";
 import Header from "../Header";
 import HomePage from "../HomePage";
 import Footer from "../Footer";
 import "./reset.scss";
 import "./styles.scss";
+import {Routes, Route} from "react-router-dom";
 
 function App() {
-	const [count, setCount] = useState(0);
-
+	
 	return (
 		<div className="App">
 			<Header />
-			<HomePage />
+				<Routes>
+					<Route path="/" element={<HomePage />}/>
+				</Routes>
 			<Footer />
 		</div>
 	);
