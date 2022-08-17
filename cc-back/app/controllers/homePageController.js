d535f99fcd8cb8bac5ad52d7705fec334812a937
Matthieu-Simon <simon.matthieu89@gmail.com>
2022-08-17 16:17:30 +0200

    add Clients & routes

---
+++ cc-back/app/controllers/homePageController.js
@@ -0,0 +1,5 @@
+module.exports = {
+  displayHomePage(_, res) {
+    return res.send('Hello Collector Chain');
+  },
+};
--- cc-back/app/router/index.js
+++ cc-back/app/router/index.js
@@ -2,4 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
+const homepageRouter = require('./homePage');
+// const signUpRouter = require('./sign_up');
+// const logingRouter = require('./login');
+
+router.use(homepageRouter);
+// router.use(signUpRouter);
+// router.use(loginRouter);
+
 module.exports = router;
