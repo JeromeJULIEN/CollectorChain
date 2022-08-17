c8ac97a0f165433869ba081542d0f99ee4b20006
OlivierDAVID35 <olivierdavid@outlook.com>
2022-08-17 12:44:36 +0200

    react modele ope

---
+++ cc-front/store/index.jsx
@@ -0,0 +1,10 @@
+// import auth from "./middlewares/auth";
+import { createStore, applyMiddleware, compose } from "redux";
+import reducer from "./reducers";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancers = composeEnhancers();
+
+const store = createStore(reducer, enhancers);
+
+export default store;
