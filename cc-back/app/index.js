953d456a47cb3e9fed267ad54392d4eb42b64df9
loicberland <berland.loic@yahoo.fr>
2022-08-18 16:48:22 +0200

    correction cors only front server can acces

--- cc-back/app/index.js
+++ cc-back/app/index.js
@@ -1,26 +1,16 @@
 const express = require('express');
-
-const multer = require('multer');
-
-const bodyParser = multer();
 const cors = require('cors');
 
-// parse request body
-// app.use(bodyParser.json());
-
-// cors
-// app.use((req, res) => {
-//     res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-// });
+const corsOptions = {
+    origin: 'http://localhost:5173',
+};
 
 const router = require('./router');
 
 const app = express();
 
-app.use(cors());
-app.use(bodyParser.none());
+app.use(cors(corsOptions));
+// app.use(bodyParser.none());
 
 // On active le middleware pour parser le payload JSON
 app.use(express.json());
