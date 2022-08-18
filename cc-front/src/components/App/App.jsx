13f14df844b7c0471af99c4e8d5b068d97ab232d
JeromeJULIEN <102427619+JeromeJULIEN@users.noreply.github.com>
2022-08-18 11:08:58 +0200

    prepa auth + init page categories

--- cc-front/src/components/App/App.jsx
+++ cc-front/src/components/App/App.jsx
@@ -1,22 +1,28 @@
+import * as React from 'react';
+import { NextUIProvider } from '@nextui-org/react';
 import { useState } from "react";
 import Header from "../Header";
-import HomePage from "../HomePage";
 import Footer from "../Footer";
 import MenuMobile from "../MenuMobile";
 import "./reset.scss";
 import "./styles.scss";
 import {Routes, Route} from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import Categories from "../pages/Categories";
 
 function App() {
 	
 	return (
 		<div className="App">
-			<Header />
-				<Routes>
-					<Route path="/" element={<HomePage />}/>
-				</Routes>
-			<Footer />
-			<MenuMobile />
+			<NextUIProvider>
+				<Header />
+					<Routes>
+						<Route path="/" element={<HomePage />}/>
+						<Route path="/categories" element={<Categories />}/>
+					</Routes>
+				<Footer />
+				<MenuMobile />
+			</NextUIProvider>
 		</div>
 	);
 }
---
+++ cc-front/utils/axios.jsx
@@ -0,0 +1,9 @@
+// Création d'une instance axios
+
+import axios from "axios";
+
+const instance = axios.create({
+	baseURL: 'http://localhost:3001'
+})
+
+export default instance
