8f479779fd234afe50f8d06d3611447fe0cb8266
loicberland <berland.loic@yahoo.fr>
2022-08-18 10:26:27 +0200

    add function findUserByEmail add jsdoc test function

--- cc-back/app/models/coreDatamapper.js
+++ cc-back/app/models/coreDatamapper.js
@@ -1,23 +1,14 @@
-// const client = require('../clients/pg');
+const client = require('../clients/pg');
 
 module.exports = class CoreDatamapper {
-    // static async findAll() {
-    //     const result = await client.query(`
-    //         SELECT * FROM "${this.tableName}"
-    //     `);
-    //     return result.rows;
-    // }
-
-    // static async create(value) {
-    //     const preparedQuery = {
-    //         text: `
-    //             INSERT INTO "${this.tableName}" ("description")
-    //             VALUES ($1)
-    //         `,
-    //         values: [value],
-    //     };
-
-    //     const result = await client.query(preparedQuery);
-    //     return result.rows[0];
-    // }
+    /**
+     * query for find all table
+     * @returns all in database contained in tableName
+     */
+    static async findAll() {
+        const result = await client.query(`
+            SELECT * FROM "${this.tableName}"
+        `);
+        return result.rows;
+    }
 };
--- cc-back/app/models/user.js
+++ cc-back/app/models/user.js
@@ -1,5 +1,25 @@
+const client = require('../clients/pg');
 const CoreDatamapper = require('./coreDatamapper');
+
+/**
+ * @typedef {object} User
+ * @property {number} id - Primary key
+ * @property {string} email
+ * @property {string} nickname
+ * @property {string} password - encrypted
+ * @property {number} wallet - actual state of the personnal wallet
+ * @property {boolean} isAdmin - egal true if this user is an admin
+ * @property {string} media - link to user picture
+ */
 
 module.exports = class User extends CoreDatamapper {
     static tableName = 'user';
+
+    static async findUserByEmail(email) {
+        const result = await client.query(
+            'SELECT * FROM "user" WHERE email = $1',
+            [email],
+        );
+        return result.rows[0];
+    }
 };
