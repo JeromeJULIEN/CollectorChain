4874c01f83d3a658a1f5bd39f7e6cedc5f0dd60e
loicberland <berland.loic@yahoo.fr>
2022-08-19 15:10:53 +0200

    test JWT done

--- cc-back/app/Auth/jwt.js
+++ cc-back/app/Auth/jwt.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const generateJwt = require('./generateJWT');
 
@@ -7,7 +8,7 @@ module.exports = {
     createToken(user) {
         const expiresIn = process.env.EXPIRES_TOKEN;
         const accessToken = generateJwt.generateAccessToken(user);
-        const refreshToken = jwt.sign(user, process.env.REFRESS_TOKEN_SECRET);
+        const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
         refreshTokens.push(refreshToken);
         return {
             accessToken, refreshToken, expiresIn,
@@ -16,33 +17,38 @@ module.exports = {
     authenticateToken(req, res, next) {
         const authHeader = req.headers.authorization;
         const token = authHeader && authHeader.split(' ')[1];
-        if (token !== null) {
+        if (token !== undefined) {
             try {
                 jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-                next();
+                return next();
             } catch (error) {
-                return res.sendStatus(403).send('Access denied');
+                return next(error.message);
             }
         }
-        return res.sendStatus(401).send('Token is null');
+        return next('Token is undefined');
     },
-    refreshToken(req, res) {
+    refreshToken(req, res, next) {
         const refreshToken = req.body.token;
-        const expiresIn = process.env.REFRESS_TOKEN_SECRET;
-        if (refreshToken == null) return res.sendStatus(401).send('Token is null');
-        if (refreshTokens.includes(refreshToken)) {
-            try {
-                const user = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
-                const accessToken = generateJwt.generateAccessToken(user);
-                res.json({ accessToken, expiresIn });
-            } catch (error) {
-                return res.sendStatus(403).send('Access denied');
+        const expiresIn = process.env.EXPIRES_TOKEN;
+        if (refreshToken !== '') {
+            if (refreshTokens.includes(refreshToken)) {
+                try {
+                    const user = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+                    delete user.iat;
+                    const accessToken = generateJwt.generateAccessToken(user);
+                    return res.json({ accessToken, expiresIn });
+                } catch (error) {
+                    return next(error.message);
+                }
+            } else {
+                return next('refreshToken not existed');
             }
+        } else {
+            return next('Token is null');
         }
-        return res.sendStatus(403).send('Access denied');
     },
-    deleteRefreshToken(req, res) {
+    deleteRefreshToken(req, res, next) {
         refreshTokens = refreshTokens.filter((token) => token !== req.body.token);
-        res.sendStatus(204).send('Refresh token deleted');
+        return next('Refresh token deleted');
     },
 };
--- cc-back/app/router/index.js
+++ cc-back/app/router/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
 const router = express.Router();
+const jwtModules = require('../Auth/jwt');
+const homePageController = require('../controllers/homePageController');
 
 const homepageRouter = require('./homePage');
 const signUpRouter = require('./sign_up');
@@ -9,5 +11,9 @@ const loginRouter = require('./login');
 router.use(homepageRouter);
 router.use(signUpRouter);
 router.use(loginRouter);
+
+router.get('/posts', jwtModules.authenticateToken, homePageController.displayHomePage);
+router.post('/token', jwtModules.refreshToken);
+router.post('/logout', jwtModules.deleteRefreshToken);
 
 module.exports = router;
