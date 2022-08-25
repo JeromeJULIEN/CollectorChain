0eb1c7dd6bb9b5cb9fa9ea2533b39fdef43975d8
JeromeJULIEN <102427619+JeromeJULIEN@users.noreply.github.com>
2022-08-25 14:30:04 +0200

    createNft2 : gestion customProp avec reducer OK

--- cc-front/store/reducers/createNft.jsx
+++ cc-front/store/reducers/createNft.jsx
@@ -1,20 +1,44 @@
-import { STORE_TEMP_PICTURE } from "../actions/createNft";
+import { DELETE_PROPERTY, STORE_PROPERTY, STORE_TEMP_PICTURE } from "../actions/createNft";
 
 export const initialState = {
     name: "",
 	media: "",
 	tempMedia: "",
+    properties:[],
+    category:'',
+    collection:'',
+    model:'',
+    serial:'',
 }
 
 const reducer = (state = initialState, action = {}) => {
 	switch (action.type) {
 		case STORE_TEMP_PICTURE: {
-			console.log("entrée dans storeTempPicture avec", action.payload);
 			return {
 				...state,
 				tempMedia: action.payload,
 				}			
 		};
+
+        case STORE_PROPERTY:{
+            return {
+                ...state,
+                properties :[
+                    ...state.properties,
+                    {name:action.property, tag:action.tag}
+                ]
+                
+            }
+        };
+
+        case DELETE_PROPERTY:{
+            console.log('entrée dans deleteProperty()', action.property)
+            return {
+                ...state,
+                properties: state.properties.filter(property => property.name !== action.property)
+            }
+
+        }
 
 		default:
 			return state;
