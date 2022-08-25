63d551248c22d08bb2a4efb301e852c9857ab6e4
JeromeJULIEN <102427619+JeromeJULIEN@users.noreply.github.com>
2022-08-25 16:23:59 +0200

    process create NFT OK avec reducer

--- cc-front/store/actions/createNft.jsx
+++ cc-front/store/actions/createNft.jsx
@@ -1,6 +1,31 @@
 export const STORE_TEMP_PICTURE = 'STORE_TEMP_PICTURE';
 export const STORE_PROPERTY = 'STORE_PROPERTY';
 export const DELETE_PROPERTY = 'DELETE_PROPERTY';
+export const STORE_NFT_MEDIA = 'STORE_NFT_MEDIA';
+export const DELETE_NFT_MEDIA = 'DELETE_NFT_MEDIA';
+export const STORE_DESCRIPTION = 'STORE_DESCRIPTION';
+export const STORE_CATEGORY = 'STORE_CATEGORY';
+export const CHANGE_NFT_FIELD = 'CHANGE_NFT_FIELD';
+
+export const changeNftField = (value,name) => ({
+  type: CHANGE_NFT_FIELD,
+  value,
+  name
+});
+
+export const storeCategory = (payload) => ({
+  type: STORE_CATEGORY,
+  payload
+});
+
+export const deleteNftMedia = () => ({
+  type: DELETE_NFT_MEDIA
+});
+
+export const storeNftMedia = (payload) => ({
+  type: STORE_NFT_MEDIA,
+  payload
+});
 
 export const deleteProperty = (property) => ({
   type: DELETE_PROPERTY,
