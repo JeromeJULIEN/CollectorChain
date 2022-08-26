b72effc679ba7ee28aec38e8a35b52674f4ab32b
JeromeJULIEN <102427619+JeromeJULIEN@users.noreply.github.com>
2022-08-26 16:17:49 +0200

    modif showcase

--- cc-front/src/components/pages/Showcase/ShowcaseNft/index.jsx
+++ cc-front/src/components/pages/Showcase/ShowcaseNft/index.jsx
@@ -1,6 +1,53 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Modal } from "rsuite";
+import { setShowcaseNftDisplayed } from "../../../../../store/actions/user";
+import NftOwned from "../../../modals/NftOwned";
 import "./styles.scss";
 
-const ShowcaseNft = ({ deleteImage, id, name, showModaleNftOwned, lastNftSelected }) => {
+const ShowcaseNft = ({id, name}) => {
+
+	const dispatch = useDispatch();
+
+	const nftOwned = useSelector((state) => state.user.nftOwned);
+
+	const nftDisplayed = useSelector((state) => state.user.showcaseNftDisplayed);
+
+	const [isModaleNftOwnedVisible, setIsModaleNftOwnedVisible] = useState(false);
+	const showModaleNftOwned = () => {
+		setIsModaleNftOwnedVisible(true);
+	};
+	const hideModaleNftOwned = () => {
+		setIsModaleNftOwnedVisible(false);
+	};
+
+	const [tempNftSelected, setTempNftSelected] = useState([]);
+	const [lastNftSelected, setLastNftSelected] = useState({});
+
+	const uploadImage = (event) => {
+		// console.log("tempNFTbefore>>", tempNftSelected);
+		setTempNftSelected((tempNftSelected) => ({
+			...tempNftSelected,
+			[event.target.name]: nftOwned.find((nft) => (nft = event.target.id))
+		}));
+		setLastNftSelected(event.target.src);
+		hideModaleNftOwned();
+	};
+	
+	useEffect(() => {
+		// console.log("TempNftSelected after>>>", tempNftSelected);
+		dispatch(setShowcaseNftDisplayed(tempNftSelected))
+	},[tempNftSelected])
+
+	useEffect(() => {
+		// console.log("TempNftSelected after>>>", tempNftSelected);
+		dispatch(setShowcaseNftDisplayed(tempNftSelected));
+		// console.log("Last nft selected >>>", lastNftSelected);
+	}, [tempNftSelected]);
+
+	const deleteImage = (event) => {
+		// console.log("delete test");
+	};
 	return (
 		<div className="showcase">
 			<div className="showcase__pic">
@@ -22,6 +69,15 @@ const ShowcaseNft = ({ deleteImage, id, name, showModaleNftOwned, lastNftSelecte
 					)} */}
 				<img src={lastNftSelected} className="showcase__pic-img" alt="" />
 			</div>
+			<Modal 
+				className="modaleNftOwned" 
+				closeButton 
+				blur 
+				open={isModaleNftOwnedVisible} 
+				onClose={hideModaleNftOwned}
+				>
+					<NftOwned hideNftOwned={hideModaleNftOwned} uploadImage={uploadImage} nftOwned={nftOwned} />
+			</Modal>
 		</div>
 	);
 };
--- cc-front/src/components/pages/Showcase/index.jsx
+++ cc-front/src/components/pages/Showcase/index.jsx
@@ -6,52 +6,51 @@ import ShowcaseNft from "./ShowcaseNft";
 import SearchBarShowcase from "../../searchBars/SearchBarShowcase";
 import NftOwned from "../../modals/NftOwned";
 import { setShowcaseNftDisplayed } from "../../../../store/actions/user";
-import { useEffect } from "react";
 import SearchBarFavorites from "../../searchBars/SearchBarFavorites";
 // import "./styles.scss";
 
 const Showcase = () => {
-	const dispatch = useDispatch();
+	// const dispatch = useDispatch();
 
-	const nftOwned = useSelector((state) => state.user.nftOwned);
+	// const nftOwned = useSelector((state) => state.user.nftOwned);
 
-	const nftDisplayed = useSelector((state) => state.user.showcaseNftDisplayed);
+	// const nftDisplayed = useSelector((state) => state.user.showcaseNftDisplayed);
 
-	const [isModaleNftOwnedVisible, setIsModaleNftOwnedVisible] = useState(false);
-	const showModaleNftOwned = () => {
-		setIsModaleNftOwnedVisible(true);
-	};
-	const hideModaleNftOwned = () => {
-		setIsModaleNftOwnedVisible(false);
-	};
+	// const [isModaleNftOwnedVisible, setIsModaleNftOwnedVisible] = useState(false);
+	// const showModaleNftOwned = () => {
+	// 	setIsModaleNftOwnedVisible(true);
+	// };
+	// const hideModaleNftOwned = () => {
+	// 	setIsModaleNftOwnedVisible(false);
+	// };
 
-	const [tempNftSelected, setTempNftSelected] = useState([]);
-	const [lastNftSelected, setLastNftSelected] = useState({});
+	// const [tempNftSelected, setTempNftSelected] = useState([]);
+	// const [lastNftSelected, setLastNftSelected] = useState({});
 
-	const uploadImage = (event) => {
-		// console.log("tempNFTbefore>>", tempNftSelected);
-		setTempNftSelected((tempNftSelected) => ({
-			...tempNftSelected,
-			[event.target.name]: nftOwned.find((nft) => (nft = event.target.id))
-		}));
-		setLastNftSelected(event.target.src);
-		hideModaleNftOwned();
-	};
+	// const uploadImage = (event) => {
+	// 	// console.log("tempNFTbefore>>", tempNftSelected);
+	// 	setTempNftSelected((tempNftSelected) => ({
+	// 		...tempNftSelected,
+	// 		[event.target.name]: nftOwned.find((nft) => (nft = event.target.id))
+	// 	}));
+	// 	setLastNftSelected(event.target.src);
+	// 	hideModaleNftOwned();
+	// };
 	
-	useEffect(() => {
-		console.log("TempNftSelected after>>>", tempNftSelected);
-		dispatch(setShowcaseNftDisplayed(tempNftSelected))
-	},[tempNftSelected])
+	// useEffect(() => {
+	// 	console.log("TempNftSelected after>>>", tempNftSelected);
+	// 	dispatch(setShowcaseNftDisplayed(tempNftSelected))
+	// },[tempNftSelected])
 
-	useEffect(() => {
-		// console.log("TempNftSelected after>>>", tempNftSelected);
-		dispatch(setShowcaseNftDisplayed(tempNftSelected));
-		console.log("Last nft selected >>>", lastNftSelected);
-	}, [tempNftSelected]);
+	// useEffect(() => {
+	// 	// console.log("TempNftSelected after>>>", tempNftSelected);
+	// 	dispatch(setShowcaseNftDisplayed(tempNftSelected));
+	// 	console.log("Last nft selected >>>", lastNftSelected);
+	// }, [tempNftSelected]);
 
-	const deleteImage = (event) => {
-		// console.log("delete test");
-	};
+	// const deleteImage = (event) => {
+	// 	// console.log("delete test");
+	// };
 
 	return (
 		<main>
@@ -66,26 +65,26 @@ const Showcase = () => {
 			<Grid fluid>
 				<Row className="show-grid">
 					<Col xs={12} sm={10} md={8}>
-						<ShowcaseNft deleteImage={deleteImage} showModaleNftOwned={showModaleNftOwned} id={1} name="nft1" lastNftSelected={lastNftSelected} />
-						<ShowcaseNft deleteImage={deleteImage} showModaleNftOwned={showModaleNftOwned} id={2} name="nft2" lastNftSelected={lastNftSelected} />
-						<ShowcaseNft deleteImage={deleteImage} showModaleNftOwned={showModaleNftOwned} id={3} name="nft3" lastNftSelected={lastNftSelected} />
-						<ShowcaseNft deleteImage={deleteImage} showModaleNftOwned={showModaleNftOwned} id={4} name="nft4" lastNftSelected={lastNftSelected} />
-						<ShowcaseNft deleteImage={deleteImage} showModaleNftOwned={showModaleNftOwned} id={5} name="nft5" lastNftSelected={lastNftSelected} />
-						<ShowcaseNft deleteImage={deleteImage} showModaleNftOwned={showModaleNftOwned} id={6} name="nft6" lastNftSelected={lastNftSelected} />
+						<ShowcaseNft   id={1} name="nft1"  />
+						<ShowcaseNft   id={2} name="nft2"  />
+						<ShowcaseNft   id={3} name="nft3"  />
+						<ShowcaseNft   id={4} name="nft4"  />
+						<ShowcaseNft   id={5} name="nft5"  />
+						<ShowcaseNft   id={6} name="nft6"  />
 					</Col>
 					<Col xs={12} sm={10} md={8}>
-						<ShowcaseNft deleteImage={deleteImage} showModaleNftOwned={showModaleNftOwned} id={7} name="nft7" />
-						<ShowcaseNft deleteImage={deleteImage} showModaleNftOwned={showModaleNftOwned} id={8} name="nft8" />
-						<ShowcaseNft deleteImage={deleteImage} showModaleNftOwned={showModaleNftOwned} id={9} name="nft9" />
-						<ShowcaseNft deleteImage={deleteImage} showModaleNftOwned={showModaleNftOwned} id={10} name="nft10" />
-						<ShowcaseNft deleteImage={deleteImage} showModaleNftOwned={showModaleNftOwned} id={11} name="nft11" />
-						<ShowcaseNft deleteImage={deleteImage} showModaleNftOwned={showModaleNftOwned} id={12} name="nft13" />
+						<ShowcaseNft   id={7} name="nft7" />
+						<ShowcaseNft   id={8} name="nft8" />
+						<ShowcaseNft   id={9} name="nft9" />
+						<ShowcaseNft   id={10} name="nft10" />
+						<ShowcaseNft   id={11} name="nft11" />
+						<ShowcaseNft   id={12} name="nft13" />
 					</Col>
 				</Row>
 			</Grid>
-			<Modal className="modaleNftOwned" closeButton blur open={isModaleNftOwnedVisible} onClose={hideModaleNftOwned}>
+			{/* <Modal className="modaleNftOwned" closeButton blur open={isModaleNftOwnedVisible} onClose={hideModaleNftOwned}>
 				<NftOwned hideNftOwned={hideModaleNftOwned} uploadImage={uploadImage} nftOwned={nftOwned} />
-			</Modal>
+			</Modal> */}
 		</main>
 	);
 };
