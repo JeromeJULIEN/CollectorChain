f2fd25a3630a16100d3a345709516fdeef72032f
loicberland <berland.loic@yahoo.fr>
2022-08-29 16:17:36 +0200

    remove dist

--- cc-back/app/index.js
+++ cc-back/app/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/dist')));
+app.use(express.static(path.resolve(__dirname, '../../cc-front/dist')));
 
 // api calls
 app.get('/api', (req, res) => {
@@ -30,7 +30,7 @@ app.get('/api', (req, res) => {
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../cc-front/dist', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../../cc-front/dist', 'index.html'));
 });
 
 app.use(router);
