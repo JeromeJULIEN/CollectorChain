69d78e4b40f5f90f5f7f9e9e12f4077541744c85
loicberland <berland.loic@yahoo.fr>
2022-08-29 16:02:29 +0200

    add app.get for deploy heroku

--- cc-back/app/index.js
+++ cc-back/app/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const errorHandler = require('./helper/errorHandler');
 const ApiError = require('./errors/apiError');
@@ -11,14 +12,26 @@ const router = require('./router');
 
 const app = express();
 
-app.use(express.static('client/build'));
-
 app.use(cors(corsOptions));
 
 // On active le middleware pour parser le payload JSON
 app.use(express.json());
 // On active le middleware pour parser le payload urlencoded
 app.use(express.urlencoded({ extended: true }));
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/dist')));
+
+// api calls
+app.get('/api', (req, res) => {
+    // use secret api keys with process.env.MY_SECRET_FAKE_API_KEY
+    res.json({ message: 'I shouldn\'t be showing you this' });
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../cc-front/dist', 'index.html'));
+});
 
 app.use(router);
 
