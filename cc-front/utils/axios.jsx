4cd3c6f5e6e6dfad0b62b1d5c016eb1c78e60782
loicberland <berland.loic@yahoo.fr>
2022-08-29 17:01:51 +0200

    axios

--- cc-front/utils/axios.jsx
+++ cc-front/utils/axios.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 
 const instance = axios.create({
 	// baseURL: 'http://localhost:5000'
-	baseURL: process.env.DATABASE_URL || 'http://localhost:5000'
+	baseURL: process.env.DATABASE_URL
 })
 
 export default instance
