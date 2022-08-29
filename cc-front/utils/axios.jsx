894f7fbc545215fea1833158cd34f947db22255d
loicberland <berland.loic@yahoo.fr>
2022-08-29 16:55:00 +0200

    change axios local

--- cc-front/utils/axios.jsx
+++ cc-front/utils/axios.jsx
@@ -3,7 +3,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-	baseURL: 'http://localhost:5000'
+	// baseURL: 'http://localhost:5000'
+	baseURL: process.env.DATABASE_URL || 'http://localhost:5000'
 })
 
 export default instance
