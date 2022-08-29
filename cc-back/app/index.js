bd1f1551e024b1b8ec2c80ee9ee54c7ca63959d2
loicberland <berland.loic@yahoo.fr>
2022-08-29 16:25:59 +0200

    change router api and website

--- cc-back/app/index.js
+++ cc-back/app/index.js
@@ -22,17 +22,6 @@ app.use(express.urlencoded({ extended: true }));
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../../cc-front/dist')));
 
-// api calls
-app.get('/api', (req, res) => {
-    // use secret api keys with process.env.MY_SECRET_FAKE_API_KEY
-    res.json({ message: 'I shouldn\'t be showing you this' });
-});
-
-// All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../../cc-front/dist', 'index.html'));
-});
-
 app.use(router);
 
 app.use((req, res, next) => {
