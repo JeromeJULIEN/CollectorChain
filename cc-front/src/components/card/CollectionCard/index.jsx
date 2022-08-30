a9788d25f6f8ae6599b364b109a6a9ad17e883c4
OlivierDAVID35 <olivierdavid@outlook.com>
2022-08-30 10:58:11 +0200

    resolve conflits

--- cc-front/src/components/card/CollectionCard/index.jsx
+++ cc-front/src/components/card/CollectionCard/index.jsx
@@ -1,13 +1,8 @@
 import { Link } from "react-router-dom";
 import "./styles.scss";
 
-<<<<<<< HEAD
 const CollectionCard = ({ media, text, id }) => {
-=======
-const CollectionCard = ({ media, text,id }) => {
-
 	// console.log(id)
->>>>>>> olivier
 	return (
 		<Link to={`/collection/${id}`} className="collectionCard">
 			<img className="collectionCard__image" src={media} alt={text} />
