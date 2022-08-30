7829b682c18667d7d39923c7721f0801d81491f2
Matthieu-Simon <simon.matthieu89@gmail.com>
2022-08-30 10:44:30 +0200

    nodemailer

--- cc-back/app/services/nodeMailer.js
+++ cc-back/app/services/nodeMailer.js
@@ -1,7 +1,35 @@
 const nodemailer = require('nodemailer');
-require('dotenv').config();
 
-async function forgottenPassword() {
+async function forgottenPassword(userEmail, password) {
+    const transporter = nodemailer.createTransport({
+        auth: {
+            user: 'test',
+            password: 'password',
+        },
+    });
+
+    const mailDescription = {
+        from: 'mailAdmin',
+        to: userEmail,
+        subject: 'Réinitialiser son mot de passe',
+        text: `
+                Bonjour !
+
+                Suite à votre demande, vous trouverez ci-joint le mot de passe
+                provisoire : ${password}, veuillez le changer lors de votre connexion dans votre profil.
+                Cordialement.
+
+                L'équipe Collector Chain
+      `,
+    };
+
+    transporter.verify(mailDescription, (err, success) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Email sent successfully !');
+        }
+    });
 }
 
 module.exports = forgottenPassword;
