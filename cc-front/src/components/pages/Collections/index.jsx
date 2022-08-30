8a5115ac2aaa48cf6b571975cf81e5eab3c22379
OlivierDAVID35 <olivierdavid@outlook.com>
2022-08-30 10:43:42 +0200

    function sort

--- cc-front/src/components/pages/Collections/index.jsx
+++ cc-front/src/components/pages/Collections/index.jsx
@@ -2,59 +2,64 @@ import CollectionCard from "../../card/CollectionCard";
 import SearchBarCollections from "../../searchBars/SearchBarCollectionsByCategory";
 
 import React, { useState, useRef, useEffect, useCallback } from "react";
-import useFetch from "../../hooks/useFetchWithInput";
-import { Loader } from "rsuite";
+// import useFetch from "../../hooks/useFetchWithInput";
+// import { Loader } from "rsuite";
 
 import "./styles.scss";
 import { useSelector } from "react-redux";
 
 const Collections = () => {
-	// Infinite scroll
-	const route = "/collections";
-	const limit = 20;
+	// // Infinite scroll
+	// const route = "/collections";
+	// const limit = 20;
 
-	const [query, setQuery] = useState("");
-	const [page, setPage] = useState(0);
-	//! j'ai enlevé le 'list' du useFetch car ca buggé pour charger les images de la BDD
-	const { loading, error } = useFetch(query, page, route, limit);
-	const loader = useRef(null);
-
-	const list = useSelector(state => state.collections.list)
+	// const [query, setQuery] = useState("");
+	// const [page, setPage] = useState(0);
+	// const { loading, error, list } = useFetch(query, page, route, limit);
+	// const loader = useRef(null);
 
 	// // Si searchBar input
 	// const handleChange = (e) => {
 	// 	setQuery(e.target.value);
 	// };
 
-	const handleObserver = useCallback((entries) => {
-		const target = entries[0];
-		if (target.isIntersecting) {
-			setPage((prev) => prev + 1);
-		}
-	}, []);
+	// const handleObserver = useCallback((entries) => {
+	// 	const target = entries[0];
+	// 	if (target.isIntersecting) {
+	// 		setPage((prev) => prev + 1);
+	// 	}
+	// }, []);
 
-	useEffect(() => {
-		const option = {
-			root: null,
-			rootMargin: "20px",
-			threshold: 0,
-		};
-		const observer = new IntersectionObserver(handleObserver, option);
-		if (loader.current) observer.observe(loader.current);
-		console.log(list.length);
-	}, [handleObserver]);
+	// useEffect(() => {
+	// 	const option = {
+	// 		root: null,
+	// 		rootMargin: "20px",
+	// 		threshold: 0,
+	// 	};
+	// 	const observer = new IntersectionObserver(handleObserver, option);
+	// 	if (loader.current) observer.observe(loader.current);
+	// 	console.log(list.length);
+	// }, [handleObserver]);
+
+	const list = useSelector((state) => state.collections.list);
+
+	const [sortList, setSortList] = useState([...list]);
 
 	// SearchBar Order by
-	const sortPrice0to1 = (event) => {
-		event.preventDefault();
-		// const sortPrice0to1 = list.sort((a, b) => a.price - b.price);
+
+	// useEffect(() => {
+	const sortPrice0to1 = () => {
+		const sortPrice0to1 = sortList.sort((a, b) => a.name - b.name);
+		setSortList(sortPrice0to1);
 		console.log("Low_To_High");
 	};
-	const sortPrice1to0 = (event) => {
-		event.preventDefault();
+	const sortPrice1to0 = () => {
 		// const sortPrice1to0 = list.sort((a, b) => b.price - a.price);
 		console.log("High_To_Low");
 	};
+	// }, [sortList]);
+	console.log("SortList>>>", sortList);
+	console.log("List>>>", list);
 
 	return (
 		<div className="collections">
@@ -63,13 +68,13 @@ const Collections = () => {
 			</div>
 			<SearchBarCollections sortPrice0to1={sortPrice0to1} sortPrice1to0={sortPrice1to0} />
 			<div className="collections__list">
-				{list.map((collection) => (
-					<CollectionCard key={collection.id} media={collection.media} text={collection.name} id={collection.id}/>
+				{sortList.map((collection) => (
+					<CollectionCard key={collection.id} media={collection.media} text={collection.name} id={collection.id} />
 				))}
 
-				{loading && <Loader content="Loading..." />}
+				{/* {loading && <Loader content="Loading..." />}
 				{error && <p>Error!</p>}
-				<div ref={loader} />
+				<div ref={loader} /> */}
 			</div>
 		</div>
 	);
--- cc-front/store/reducers/collections.jsx
+++ cc-front/store/reducers/collections.jsx
@@ -1,16 +1,16 @@
 import { SET_COLLECTIONS } from "../actions/data";
 
 export const initialState = {
-	list: ["Rolex", "Breitling", "Baume & Mercier", "Bell & Ross", "test5", "test6", "test7", "test8", "test9", "test10", "test11", "test12"],
+	list: [],
 };
 
 const reducer = (state = initialState, action = {}) => {
 	switch (action.type) {
-		case SET_COLLECTIONS:{
-			return{
+		case SET_COLLECTIONS: {
+			return {
 				...state,
-				list:action.payload
-			}
+				list: action.payload,
+			};
 		}
 		default:
 			return state;
