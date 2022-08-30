aa8cd49792c17554e31a8832d77e283a08fec66a
OlivierDAVID35 <olivierdavid@outlook.com>
2022-08-30 10:55:16 +0200

    resolve conflits

--- cc-front/utils/axios.jsx
+++ cc-front/utils/axios.jsx
@@ -3,13 +3,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-<<<<<<< HEAD
-	baseURL: "http://localhost:5000",
-});
-=======
 	// baseURL: 'http://localhost:5000'
-	baseURL: 'https://collector-chain.herokuapp.com'
-})
->>>>>>> f8cd71493ec15145520e4cb3e0da764c543234f0
+	baseURL: "https://collector-chain.herokuapp.com",
+});
 
 export default instance;
