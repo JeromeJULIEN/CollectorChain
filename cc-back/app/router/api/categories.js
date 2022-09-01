3d488b98c20480e6989303f313e8809fdd8a5cfc
Matthieu-Simon <simon.matthieu89@gmail.com>
2022-09-01 11:47:01 +0200

    jsdoc route categories

--- cc-back/app/router/api/categories.js
+++ cc-back/app/router/api/categories.js
@@ -5,10 +5,37 @@ const router = express.Router();
 const categoriesController = require('../../controllers/categoriesController');
 
 const controllerHandler = require('../../helper/controllerHandler');
-
+/**
+ * GET /categories
+ * @summary Route to all categories
+ * @param {string} tableName - category tableName
+ */
 router.get('/categories', controllerHandler(categoriesController.getAllCategories));
+/**
+ * POST /categories
+ * @summary Route to categories
+ * @param {string} name - category name
+ * @param {string} description - category description
+ * @param {string} media - category media
+ * @return {categories} 200 - success response - application/json
+ */
 router.post('/categories', controllerHandler(categoriesController.createCategorie));
+/**
+ * DELETE /categories/:id
+ * @summary Route to categories/:id
+ * @param {number} id - id from category deleted
+ * @return {categories} 200 - success response - application/json
+ */
 router.delete('/categories/:id', controllerHandler(categoriesController.deleteCategorie));
+/**
+ * UPDATE /categories/:id
+ * @summary Route to categories
+ * @param {number} id - id from category to update
+ * @param {string} name - category name
+ * @param {string} description - category description
+ * @param {string} media - category media
+ * @return {categories} 200 - success response - application/json
+ */
 router.patch('/categories/:id', controllerHandler(categoriesController.updateCategories));
 
 module.exports = router;
