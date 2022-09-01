a3b44108c8abb0f46d216327972961a0ffc2640f
Matthieu-Simon <simon.matthieu89@gmail.com>
2022-09-01 13:33:21 +0200

    jsdoc route favorite

--- cc-back/app/router/api/favorite.js
+++ cc-back/app/router/api/favorite.js
@@ -5,8 +5,27 @@ const router = express.Router();
 const favoriteController = require('../../controllers/favoriteController');
 const controllerHandler = require('../../helper/controllerHandler');
 
+/**
+ * GET /favorite/:id
+ * @summary Route to favorite
+ * @param {number} id - user_id
+ */
 router.get('/favorite/:id', controllerHandler(favoriteController.getAllFavorite));
+/**
+ * POST //favorite/:id_user/:id_nft
+ * @summary Route to add favorite nft
+ * @param {number} id - id from nft added
+ * @param {number} id - id from user
+ * @return {Object} 200 - success response - application/json
+ */
 router.post('/favorite/:id_user/:id_nft', controllerHandler(favoriteController.addFavorite));
+/**
+ * DELETE /favorite/:id_user/:id_nft
+ * @summary Route to delete favorite nft
+ * @param {number} id - id from nft deleted
+ * @param {number} id - id from user
+ * @return {string} 200 - success response - application/json
+ */
 router.delete('/favorite/:id_user/:id_nft', controllerHandler(favoriteController.deleteFavorite));
 
 module.exports = router;
