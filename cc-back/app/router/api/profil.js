af1300ba63a1bd26544dcbb09444cb5720d0017d
Matthieu-Simon <simon.matthieu89@gmail.com>
2022-09-01 14:15:43 +0200

    jsdoc route profil

--- cc-back/app/router/api/profil.js
+++ cc-back/app/router/api/profil.js
@@ -8,8 +8,36 @@ const controllerHandler = require('../../helper/controllerHandler');
 const { profilId, updateProfil } = require('../../validation/schemas/user');
 const validation = require('../../validation/validator');
 
+/**
+ * GET /profil/:id
+ * @summary Route to get user
+ * @tags user
+ * @param {number} id - profil user by id
+ * @return {string} 200 - success response - application/json
+ */
 router.get('/profil/:id', validation('params', profilId), controllerHandler(userController.getUser));
+/**
+ * DELETE /profil/:id/delete
+ * @summary Route to delete user
+ * @tags user
+ * @param {number} id - profil user by id
+ * @return {string} 200 - success response - application/json
+ */
 router.delete('/profil/:id/delete', controllerHandler(userController.deleteProfilUser));
+/**
+ * UPDATE /profil/:id/update
+ * @summary Route to update user
+ * @tags user
+ * @param {number} id - user id
+ * @param {string} email - user email
+ * @param {string} nickname - user nickname
+ * @param {string} password - user password
+ * @param {number} wallet - user wallet
+ * @param {string} media - user media
+ * @param {string} name - user name
+ * @param {string} lastname - user lastname
+ * @return {Object} 200 - success response - application/json
+ */
 router.patch('/profil/:id/update', validation('body', updateProfil), controllerHandler(userController.updateUserProfile));
 
 module.exports = router;
