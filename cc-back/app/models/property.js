728f44c41da3699304d136ee9f7f6e623f63d253
loicberland <berland.loic@yahoo.fr>
2022-09-01 10:14:40 +0200

    property and tag rout fixed

--- cc-back/app/models/property.js
+++ cc-back/app/models/property.js
@@ -1,27 +1,5 @@
-const client = require('../clients/pg');
 const CoreDatamapper = require('./coreDatamapper');
 
 module.exports = class Property extends CoreDatamapper {
     static tableName = 'property';
-
-    static async getByPropertyId(id) {
-        const result = await client.query(
-            `SELECT * FROM "property"
-          WHERE "id" = $1
-          `,
-            [id],
-        );
-        return result.rows;
-    }
-
-    static async getByPropertyIdLimit(id, limit) {
-        const result = await client.query(
-            `SELECT * FROM "property"
-          WHERE "id" = $1
-          LIMIT $2
-          `,
-            [id, limit],
-        );
-        return result.rows;
-    }
 };
--- cc-back/app/models/tag.js
+++ cc-back/app/models/tag.js
@@ -1,27 +1,5 @@
-const client = require('../clients/pg');
 const CoreDatamapper = require('./coreDatamapper');
 
 module.exports = class Tag extends CoreDatamapper {
     static tableName = 'tag';
-
-    static async getByTagId(id) {
-        const result = await client.query(
-            `SELECT * FROM "tag"
-          WHERE "id" = $1
-          `,
-            [id],
-        );
-        return result.rows;
-    }
-
-    static async getByTagIdLimit(id, limit) {
-        const result = await client.query(
-            `SELECT * FROM "tag"
-          WHERE "id" = $1
-          LIMIT $2
-          `,
-            [id, limit],
-        );
-        return result.rows;
-    }
 };
